e30da4790c69a3a0d10e2079a69f0b021e80b8f1
Gustaw Cyburt <gcyburt@gmail.com>
2023-12-02 17:46:48 +0100

    add CalculationsGrid component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Header from './Header'
 import UserForm from './UserForm'
+import CalculationsGrid from './CalculationsGrid'
 
 function App() {
   return (
     <>
       <Header />
       <UserForm />
+      <CalculationsGrid />
     </>
   )
 }
---
+++ src/CalculationsGrid.jsx
@@ -0,0 +1,7 @@
+export default function CalculationsGrid() {
+    return (
+        <div id="result">
+            dupa
+        </div>
+    )
+}
